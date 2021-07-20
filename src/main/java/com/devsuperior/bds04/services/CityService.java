2ddf88e5d7b895cdb97b3edbac4d4d68e9e79619
Luiznt <luiznt@hotmail.com>
2021-07-20 04:48:01 -0300

    Funcionalities and validation

--- src/main/java/com/devsuperior/bds04/services/CityService.java
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.entities.City;
@@ -21,13 +24,27 @@ public class CityService {
 	@Autowired
 	private CityRepository repository; 
 
+	@Transactional(readOnly = true)
+	public Page<CityDTO> findAllPage (Pageable pageable) {
+		Page<City> page = repository.findAll(pageable);
+		return page.map(x -> new CityDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll(){
-		
 		List<City> list  = repository.findAll(Sort.by("name"));
-		
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+	
+	@Transactional
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
